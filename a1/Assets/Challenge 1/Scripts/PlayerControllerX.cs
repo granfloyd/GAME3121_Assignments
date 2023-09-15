e9e0da8ea81ac9e405a09528fdb4cc7e9de0822f
Michael Dibonaventura <92171589+granfloyd@users.noreply.github.com>
2023-09-15 18:17:50 -0400

    added step 3

--- a1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ a1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -20,10 +20,12 @@ public class PlayerControllerX : MonoBehaviour
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
 
+        
         // move the plane forward at a constant rate
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * verticalInput * rotationSpeed * Time.deltaTime);
+
     }
 }
